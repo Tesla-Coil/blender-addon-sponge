e3d7fea1149c7029e9a2e77679eb12a435ef1109
nomelif <theo.friberg@gmail.com>
2014-01-20 17:35:42 +0200

    Initial base of the code

---
+++ sponge.py
@@ -0,0 +1,10 @@
+bl_info = {"name": "Sponge mesh",
+"category": "Mesh",
+"description": "Addon turning meshes into crystaline structures",
+"author": "Théo Friberg",
+"version": (1, 0)
+}
+def register():
+    print("Hello World")
+def unregister():
+    print("Goodbye World")
